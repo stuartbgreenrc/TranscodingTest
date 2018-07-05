f00f4e3e603e79ecc30d29757073bd260af2ea13
stuartbgreenrc <stuartgreen@rockconnections.com>
2018-07-05 10:13:33 -0400

    Port change

--- TranscodingTest/app.ts
+++ TranscodingTest/app.ts
@@ -47,7 +47,7 @@ app.use((err: any, req, res, next) => {
     });
 });
 
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
